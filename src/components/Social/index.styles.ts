cbd1d3a885a973cd8b7608dd91844a15524d224a
felipe fama <91050670+FelipeFama@users.noreply.github.com>
2023-04-14 13:12:29 -0300

    Update styles SocialContainer component

--- src/components/Social/index.styles.ts
+++ src/components/Social/index.styles.ts
@@ -6,4 +6,9 @@ export const SocialContainer = styled.div`
   width: 100%;
   gap: 1.5rem;
   margin-bottom: 3rem;
+  @media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 3rem;
+  }
 `;
